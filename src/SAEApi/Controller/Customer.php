1c2bda9c7e27983703eb516184c9f93b6d350abc
Dario Magliocchetti <dario_dml@hotmail.com>
2013-09-15 14:52:10 +0100

    Customer view working with real data

--- src/SAEApi/Controller/Customer.php
+++ src/SAEApi/Controller/Customer.php
@@ -13,28 +13,26 @@ class Customer implements ControllerProviderInterface {
     {
         $c = $app['controllers_factory'];
 
+        $c->get('/{customerid}', function (Application $app, $customerid) {
+            $text = $app['db']->fetchAssoc('SELECT * FROM customers WHERE customerID = ?', array($customerid));
 
-        $c->get('/', function (Application $app) {
-            $app->abort('404', "Customer ID missing!");
-        });
+            if ($text === false) {
+                $app->abort('404', "invalid customer id");
+            } else {
+                $text['codes'] = $app['db']->fetchAssoc('SELECT * FROM codes WHERE boxID = ?', array($text['boxID']));
+                return json_encode($text);
+            }
+        })->value('customerid', '0');
 
-        $c->get('/{customerid}', function ($customerid) {
-            $text = array(
-                '50'=>array(
-                    "name" => "Test Customer",
-                    "address" => "Imperial College Road, SW72AZ",
-                    "boxID" => "s00001",
-                    "codes" => array(
-                        "14241348","98574839","39481750"),
-                    "totalpay" => "1374",
-                    "freedays" => "0"
-                )
-            );
-            return json_encode($text);
-        });
+        $c->match('/{customerid}/codes', function(Application $app, $customerid) {
+            $text = $app['db']->fetchAssoc('SELECT * FROM customers WHERE customerID = ?', array($customerid));
 
-        $c->post('/{customerid}/codes', function(Application $app) {
-            return json_encode(array("success" => "true", "code" => "15487451", "boxID" => "s00001", "duration" => "14", "cost" => "18000"));
+            if ($text === false) {
+                $app->abort('404', "invalid customer id");
+            } else {
+                $text = $app['db']->fetchAssoc('SELECT * FROM codes WHERE boxID = ?', array($text['boxID']));
+                return json_encode($text);
+            }
         });
 
         return $c;
