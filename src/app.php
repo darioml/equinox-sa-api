362cbfbe8ea56e2c6ac0ebde9f0a5261a7efd8dc
Dario Magliocchetti <dario_dml@hotmail.com>
2013-09-15 13:56:51 +0100

    Fixed app.php

--- src/app.php
+++ src/app.php
@@ -6,12 +6,4 @@ use Symfony\Component\HttpFoundation;
 
 $app = new Application();
 
-
-$app->before(function (Request $request) {
-    if (!in_array($request->server->get("REMOTE_ADDR"), array('127.0.0.1'))) {
-        echo "Wrong IP";
-        $app->abort();
-    }
-});
-
 return $app;
